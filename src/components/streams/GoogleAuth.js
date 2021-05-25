79ca9ed35d29436e119e4499dcd234ea51cef2f6
Qodequay <rohit.patil@qodequay.com>
2021-05-25 22:40:33 +0530

    added Goole api

--- src/components/streams/GoogleAuth.js
+++ src/components/streams/GoogleAuth.js
@@ -1,17 +1,72 @@
 import React, { Component } from 'react';
 
 class GoogleAuth extends Component{
+    state={
+        isSignedIn:null
+    }
     componentDidMount(){
         window.gapi.load('client:auth2',()=>{
             window.gapi.client.init({
-                clientId:'',
+                clientId:'Your Api key here',
                 scope:'email'
-            })
+            }).then(()=>{
+             this.auth = window.gapi.auth2.getAuthInstance();
+             this.setState({
+                isSignedIn:this.auth.isSignedIn.get()
+             })
+             this.auth.isSignedIn.listen(this.onAuthChange);
+            // console.log(this.state.isSignedIn)
+            });
         });
     }
+
+     
+    onAuthChange =()=>{
+         this.setState({
+             isSignedIn:this.auth.isSignedIn.get()
+        
+        });
+    }
+
+    onSignIn=()=>{
+        this.auth.signIn()
+    }
+
+    onSignOut=()=>{
+        this.auth.signOut();
+    }
+
+    rendeAuthButton(){
+       if(this.state.isSignedIn === null){
+           return null;
+       }else if(this.state.isSignedIn) {
+        return (
+         <button 
+          onClick={this.onSignOut}
+         className="ui red google button">
+             <i className="google icon" />
+             Sign Out
+         </button>
+        );
+       }else {
+        return(
+            <button
+            onClick={this.onSignIn}
+            className="ui red google button">
+            <i className="google icon"/>
+                Sign In With Google
+            </button>
+             );
+       }
+    }
+
+
     render(){
-        return <div>Google Auth</div>;
+        return <div>{this.rendeAuthButton()}</div>;
     }
 }
 
 export default GoogleAuth;
+
+
+
